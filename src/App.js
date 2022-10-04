cef5a2a02eb72613a8ad4b3fbcbf2652a3d4dfc2
zengpang <2516629927@qq.com>
2022-10-04 23:46:26 +0800

    初次更新

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import Logo from './logo.svg';
 import './App.css';
-
+import Home from './pages/Home';
+import History from './pages/History';
+import About from './pages/About';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import React, { Suspense, lazy } from 'react';
+import {
+  Routes,
+  Link,
+  Route
+} from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={Logo}></img>
+      <nav>
+        <Link to="/">首页</Link>
+        <Link to="/history">上传历史</Link>
+        <Link to="/about">关于我</Link>
+      </nav>
+      <Header>
+
+      </Header>
+      <Routes>
+        <Route path="/" exact="true" element={<Home />}></Route>
+        <Route path="/history" element={<History />}></Route>
+        <Route path="/about" element={<About />}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+function Footer(){
+    return(
+        <>
+          <p>footer</p>
+        </>
+    );
+}
+export default Footer;
